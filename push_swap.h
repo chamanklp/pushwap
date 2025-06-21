0db7bc58a3aa6b9d677e39ada98e75c228bbcb80
chamanklp <adamvegard91@gmail.com>
2025-06-21 15:56:08 +0200

    del

--- push_swap.h
+++
@@ -1,9 +0,0 @@
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
-
-#include "utils/utils.h"
-
-int arg_checker(char *str,t_double_list    *list_a);
-void arg_list_checker(char *str,t_double_list    *list_a);
-int check_redondancy(int new_content,t_double_list *list_a);
-#endif
